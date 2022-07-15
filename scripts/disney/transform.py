025a897a6e2f7ca0f5b349b5e2f80b92a4d03755
dpasse <dpasse@mpirik.com>
2022-07-15 07:37:24 -0500

    update graph

--- scripts/disney/transform.py
+++ scripts/disney/transform.py
@@ -95,147 +95,84 @@ def clean_title(title):
     return re.sub(r'[ ]{2,}', ' ', title).strip()
 
 def get_job_level(row):
+    types = {
+        'SENIOR': [
+            r'\b(senior)\b',
+        ],
+        'PRINCIPAL': [
+            r'\b(principal)\b',
+        ],
+        'ASSOCIATE': [
+            r'\b(associate)\b',
+        ],
+        'PROJECT-HIRE': [
+            r'(project hire)',
+        ],
+        'DIRECTOR': [
+            r'\b(director)\b',
+        ],
+        'LEAD': [
+            r'\b(lead)\b',
+        ],
+        'MANAGER': [
+            r'\b(manager)\b',
+        ],
+    }
+
     keywords = []
-    title = row['full_title']
-
-    senior_expressions = [
-        r'\b(senior)\b',
-    ]
-
-    if is_a(senior_expressions, title):
-        keywords.append('SENIOR')
-
-    principal_expressions = [
-        r'\b(principal)\b',
-    ]
-
-    if is_a(principal_expressions, title):
-        keywords.append('PRINCIPAL')
-
-    associate_expressions = [
-        r'\b(associate)\b',
-    ]
-
-    if is_a(associate_expressions, title):
-        keywords.append('ASSOCIATE')
-
-    ph_expressions = [
-        r'(project hire)',
-    ]
-
-    if is_a(ph_expressions, title):
-        keywords.append('PROJECT-HIRE')
-
-    director_expressions = [
-        r'\b(director)\b',
-    ]
-
-    if is_a(director_expressions, title):
-        keywords.append('DIRECTOR')
-
-    lead_expressions = [
-        r'\b(lead)\b',
-    ]
-
-    if is_a(lead_expressions, title):
-        keywords.append('LEAD')
-
-    manager_expressions = [
-        r'\b(manager)\b',
-    ]
-
-    if is_a(manager_expressions, title):
-        keywords.append('MANAGER')
+    for label, expressions in types.items():
+        if is_a(expressions, row['full_title']):
+            keywords.append(label)
 
     return ', '.join(keywords)
 
 def get_job_type(row):
+    types = {
+        'SOFTWARE': [
+            r'\b(software)\b',
+        ],
+        'MECHANICAL': [
+            r'\b(mechanical)\b',
+        ],
+        'CIVIL': [
+            r'\b(civil)\b',
+        ],
+        'ELECTRICAL': [
+            r'\b(electrical)\b',
+        ],
+        'ELECTRONICS': [
+            r'\b(electronics?)\b',
+        ],
+        'STRUCTURAL': [
+            r'\b(structural)\b',
+        ],
+        'RESEARCH': [
+            r'\b(research)\b',
+        ],
+        'SUPERINTENDENT': [
+            r'\b(superintendent)\b',
+        ],
+        'PRODUCT': [
+            r'\b(product)\b',
+        ],
+        'PRODUCTION': [
+            r'\b(production)\b',
+        ],
+        'SYSTEMS': [
+            r'\b(systems?)\b',
+        ],
+        'ESTIMATOR': [
+            r'\b(estimator)\b',
+        ],
+        'PLANNER': [
+            r'\b(planner)\b',
+        ]
+    }
+
     jobs = []
-    title = row['full_title']
-
-    software_expressions = [
-        r'\b(software)\b',
-    ]
-
-    if is_a(software_expressions, title):
-        jobs.append('SOFTWARE')
-
-    mechanical_expressions = [
-        r'\b(mechanical)\b',
-    ]
-
-    if is_a(mechanical_expressions, title):
-        jobs.append('MECHANICAL')
-
-    electrical_expressions = [
-        r'\b(electrical)\b',
-    ]
-
-    if is_a(electrical_expressions, title):
-        jobs.append('ELECTRICAL')
-
-    electronics_expressions = [
-        r'\b(electronics?)\b',
-    ]
-
-    if is_a(electronics_expressions, title):
-        jobs.append('ELECTRONICS')
-
-    structural_expressions = [
-        r'\b(structural)\b',
-    ]
-
-    if is_a(structural_expressions, title):
-        jobs.append('STRUCTURAL')
-
-    research_expressions = [
-        r'\b(research)\b',
-    ]
-
-    if is_a(research_expressions, title):
-        jobs.append('RESEARCH')
-
-    superintendent_expressions = [
-        r'\b(superintendent)\b',
-    ]
-
-    if is_a(superintendent_expressions, title):
-        jobs.append('SUPERINTENDENT')
-
-    product_expressions = [
-        r'\b(product)\b'
-    ]
-
-    if is_a(product_expressions, title):
-        jobs.append('PRODUCT')
-
-    production_expressions = [
-        r'\b(production)\b',
-    ]
-
-    if is_a(production_expressions, title):
-        jobs.append('PRODUCTION')
-
-    systems_expressions = [
-        r'\b(systems?)\b',
-    ]
-
-    if is_a(systems_expressions, title):
-        jobs.append('SYSTEMS')
-
-    estimator_expressions = [
-        r'\b(estimator)\b',
-    ]
-
-    if is_a(estimator_expressions, title):
-        jobs.append('ESTIMATOR')
-
-    planner_expressions = [
-        r'\b(planner)\b',
-    ]
-
-    if is_a(planner_expressions, title):
-        jobs.append('PLANNER')
+    for label, expressions in types.items():
+        if is_a(expressions, row['full_title']):
+            jobs.append(label)
 
     return ', '.join(jobs)
 
